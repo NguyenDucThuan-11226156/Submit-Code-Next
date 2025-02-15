49e448006d2cd9b453423a7bfb1d2537d2f397b3
Lampx83 <phamxuanlam@gmail.com>
2025-02-15 23:41:45 +0700

    submit-code

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-// next.config.js
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/submit-code/:path*',
-        destination: 'https://submit-code-next.vercel.app/:path*', // Trỏ tới dự án submit-code đã được triển khai trên Vercel
-      },
-    ];
-  },
+// import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  /* config options here */
+  basePath: isProd ? '/submit-code' : '',
+  trailingSlash: false,
 };
+
+export default nextConfig;
