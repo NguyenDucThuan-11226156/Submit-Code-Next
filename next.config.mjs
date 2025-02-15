c7478cd2def7eefc37956a6599bfea148704087a
Lampx83 <phamxuanlam@gmail.com>
2025-02-15 23:28:50 +0700

    submit-code

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
-// next.config.mjs
 const nextConfig = {
-    basePath: '/submit-code',  // Thiết lập basepath
-    trailingSlash: false,
-  };
-  
-  export default nextConfig;
-  
+  trailingSlash: false,
+  async rewrites() {
+    return [
+      {
+        source: "/submit-code/:path*",
+        destination: "https://submit-code-next.vercel.app/:path*", // Submit Code (Thuan)
+      },
+      // Các điều hướng khác...
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
