fb039fc5709c7eefae8169bfe6b1431eda7bf1f9
NguyenDucThuan-11226156 <128773377+NguyenDucThuan-11226156@users.noreply.github.com>
2025-02-16 11:08:58 +0700

    edit

---
+++ middleware.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+export function middleware(request) {
+  const url = request.nextUrl.clone();
+  const rewrites = [
+    { path: "/submit-code", destination: "https://submit-code-next.vercel.app/" },
+    // Các điều hướng khác...
+  ];
+  for (const { path, destination } of rewrites) {
+    if (url.pathname.startsWith(path)) {
+      return NextResponse.rewrite(
+        `${destination}${url.pathname.replace(path, "")}${url.search}`
+      );
+    }
+  }
+  return NextResponse.next();
+}
