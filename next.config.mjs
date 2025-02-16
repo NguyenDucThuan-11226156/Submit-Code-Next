838227bc1daf960081a701f131b4aaa9712bc8a4
NguyenDucThuan-11226156 <128773377+NguyenDucThuan-11226156@users.noreply.github.com>
2025-02-16 11:04:38 +0700

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
+// // import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
+
+// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
+
 // const nextConfig = {
-//   basePath: process.env.NODE_ENV === 'production' ? '/submit-code' : '',
+//   /* config options here */
+//   basePath: isProd ? '/submit-code' : '',
 //   trailingSlash: false,
 // };
+const nextConfig = {
+  /* config options here */
+  basePath: isProd ? '/submit-code' : '',
+  trailingSlash: false,
+};
 
 // export default nextConfig;
+export default nextConfig;
