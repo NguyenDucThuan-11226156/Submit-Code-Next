91804494287f1037b74920cee8cdea7afc449c78
NguyenDucThuan-11226156 <128773377+NguyenDucThuan-11226156@users.noreply.github.com>
2025-03-08 22:38:52 +0700

    fix

--- src/components/codelab/EditorSubmitCode.jsx
+++ src/components/codelab/EditorSubmitCode.jsx
@@ -153,7 +153,7 @@ export default function CodeEditor() {
       fetchQuestions();
     }
   }, [roomId]);
-
+console.log(selectedSubmission)
   // Lưu code vào localStorage theo câu hỏi
   useEffect(() => {
     if (selectedQuestion) {
